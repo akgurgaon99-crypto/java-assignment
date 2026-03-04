c8772811af3a9f02eb480381bf601a145520b234
akgurgaon99-crypto <akgurgaon99@gmail.com>
2026-03-04 20:03:54 +0530

    Add files via upload

---
+++ src/test/java/com/amazon/test/AmazonTest.java
@@ -0,0 +1,84 @@
+package com.amazon.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import java.time.Duration;
+import java.util.List;
+
+public class AmazonTest {
+
+    @Test
+    public void testiPhone() {
+        setupAndRun("iPhone");
+    }
+
+    @Test
+    public void testGalaxy() {
+        setupAndRun("Galaxy");
+    }
+
+    public void setupAndRun(String product) {
+        // Chrome options for stability
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        
+        WebDriver driver = new ChromeDriver(options);
+        runTask(driver, product);
+    }
+
+    public void runTask(WebDriver driver, String product) {
+        // Wait object (10 seconds max wait)
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        org.openqa.selenium.JavascriptExecutor js = (org.openqa.selenium.JavascriptExecutor) driver;
+    //    String originalWindow = driver.getWindowHandle();
+        try {
+            driver.get("https://www.amazon.in");
+            driver.manage().window().maximize();
+
+            // 1. Wait for Search Box and Search
+            WebElement searchBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("twotabsearchtextbox")));
+            searchBox.sendKeys(product);
+            searchBox.submit();
+            
+            // 2. Wait for Add to Cart Button and Click
+            By cartLocator = By.xpath("//button[@name='submit.addToCart']");
+       //     By cartLocator = By.xpath("//input[@id='add-to-cart-button' or @name='submit.add-to-cart' or contains(@value, 'Add to Cart')]");
+            WebElement addToCartBtn = wait.until(ExpectedConditions.presenceOfElementLocated(cartLocator));
+            js.executeScript("arguments[0].click();", addToCartBtn);
+            
+            System.out.println(product + " added to cart successfully!");
+
+            // 2. Wait for Price and Print
+            WebElement priceElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("a-price-whole")));
+            String price = priceElement.getText();
+            System.out.println(product + " Price: " + price);
+
+     //            // 3. Wait for Product Link and Click
+    //        WebElement productLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@data-cy='title-recipe'])[1]//a[@target='_blank']")));
+//            productLink.click();
+//            js.executeScript("arguments[0].click();", productLink);
+
+//            wait.until(d -> d.getWindowHandles().size() > 0); 
+//            for (String windowHandle : driver.getWindowHandles()) {
+//                if (!originalWindow.contentEquals(windowHandle)) {
+//                    driver.switchTo().window(windowHandle);
+//                    System.out.println("done");
+//                    break;
+//                }
+           
+
+       
+
+        } catch (Exception e) {
+            System.out.println("Error during " + product + " test: " + e.getMessage());
+        } finally {
+          //  driver.quit();
+        }
+    }
+}
